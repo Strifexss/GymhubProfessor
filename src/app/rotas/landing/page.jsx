ff8014f15d9ae7291b36e263a4325ae88b56636e
Strifexss <rocombole0007@gmail.com>
2023-05-10 00:03:11 -0300

    #

--- src/app/rotas/landing/page.jsx
+++ src/app/rotas/landing/page.jsx
@@ -1,8 +1,9 @@
 "use client"
 import Cookies from 'js-cookies'
 
+console.log(Cookies.getItem("id"))
 export default function landing() {
-    console.log(Cookies.getItem("id"))
+    
     return(
     <div className="w-screen h-screen bg-Cinza1 flex justify-center items-center">
         <h1 className="text-[2rem] text-white">Logado</h1>
