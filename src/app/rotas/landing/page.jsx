b42351bf19babd7ff9f5ec414386cf11eb215e3b
Strifexss <rocombole0007@gmail.com>
2023-05-12 10:44:47 -0300

    #

--- src/app/rotas/landing/page.jsx
+++ src/app/rotas/landing/page.jsx
@@ -1,20 +1,15 @@
 "use client"
 import Cookies from 'js-cookies'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Landing() {
+
+    const [testes, setTeste] = useState("asdasd")
 
     return(
     <div className="w-screen h-screen bg-Cinza1 flex justify-center items-center">
         <h1 className="text-[2rem] text-white">Logado</h1>
-        {
-           typeof document != undefined &&
-            <h1 className='text-white'>
-                {
-                    Cookies.getItem("id")
-                }
-            </h1>
-        }
+        <button onClick={() => console.log(testes)}>Clica</button>
     </div>
     )
 }
