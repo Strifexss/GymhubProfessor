d30ecc5e7cc5d4736f2019be7d9db3ab604ebc0f
Strifexss <rocombole0007@gmail.com>
2023-05-12 10:34:48 -0300

    #

--- src/app/rotas/landing/page.jsx
+++ src/app/rotas/landing/page.jsx
@@ -6,6 +6,14 @@ export default function landing() {
     return(
     <div className="w-screen h-screen bg-Cinza1 flex justify-center items-center">
         <h1 className="text-[2rem] text-white">Logado</h1>
+        {
+            document != undefined &&
+            <h1 className='text-white'>
+                {
+                    Cookies.getItem("id")
+                }
+            </h1>
+        }
     </div>
     )
 }
