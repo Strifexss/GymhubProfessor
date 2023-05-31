d2ee497e3b2042bb65b56380c4c94972847b8e48
Strifexss <rocombole0007@gmail.com>
2023-05-31 18:15:52 -0300

    #

--- src/app/components/infoalunoComponents/modalInfos.jsx
+++ src/app/components/infoalunoComponents/modalInfos.jsx
@@ -2,18 +2,39 @@ import alterIcon from "../../imgs/alterIcon.png"
 import Image from "next/image"
 export default function ModalInfo(props) {
     return(
-        <div className="text-white w-[30rem] h-[40rem] bg-Cinza1 fixed rounded-2xl flex flex-col items-center p-4">
-            <div className="flex flex-row w-full items-center justify-start m-4">
-                <Image className="w-[3rem] m-4"
-                    src={alterIcon}
-                    width={35}
-                    height={30}
-                    alt="Icon"
-                />
-                <div className="flex flex-col">
-                    <h1 className="text-white font-bold text-[1.5rem]">Exercicios Segunda</h1>
-                    <p className="text-white">Visualize e gerencie os exercicios</p>
+        <div className="text-white w-[95%] border-solid border-[1px] border-RoxoPadrao md:w-[30rem] h-[48rem] bg-Cinza1 fixed rounded-2xl flex flex-col items-center p-4">
+            <div className="flex flex-row w-full justify-around">
+                <div className="flex flex-row items-center justify-start m-4">
+                    <Image className="w-[3rem] m-4"
+                        src={alterIcon}
+                        width={35}
+                        height={30}
+                        alt="Icon"
+                    />
+                    <div className="flex flex-col">
+                        <h1 className="text-white font-bold text-[1.5rem]">Exercicios {props.dia}</h1>
+                        <p className="text-white">Visualize e gerencie os exercicios</p>
+                    </div>
+                    <button className="ml-8" onClick={props.fechar}>Fechar</button>
                 </div>
+            </div>
+            <div className="w-full flex flex-col">
+            <div className="flex flex-col w-full items-start m-4">
+                <h1 className="text-white text-[1.2rem] font-bold">{props.exercicio1nome}:</h1>
+                <h1 className="text-white w-[90%] bg-black h-[6rem] mt-2 rounded-[1rem] p-4">{props.exercicio1}</h1>
+            </div>
+            <div className="flex flex-col w-full items-start m-4">
+                <h1 className="text-white text-[1.2rem] font-bold">{props.exercicio2nome}:</h1>
+                <h1 className="text-white w-[90%] bg-black h-[6rem] mt-2 rounded-[1rem] p-4">{props.exercicio2}</h1>
+            </div>
+            <div className="flex flex-col w-full items-start m-4">
+                <h1 className="text-white text-[1.2rem] font-bold">{props.exercicio3nome}:</h1>
+                <h1 className="text-white w-[90%] bg-black h-[6rem] mt-2 rounded-[1rem] p-4">{props.exercicio3}</h1>
+            </div>
+            </div>
+            <div className="w-full h-full flex flex-row items-center justify-around">
+                <button className="bg-white border-none rounded-xl w-[10rem] h-[3rem] text-black font-semibold text-[1.2rem]">Editar</button>
+                <button className="bg-RoxoPadrao border-none rounded-xl w-[10rem] h-[3rem] text-white font-semibold text-[1.2rem]">Excluir</button>
             </div>
         </div>
     )
--- src/app/rotas/aulaAluno/page.jsx
+++ src/app/rotas/aulaAluno/page.jsx
@@ -16,7 +16,7 @@ export default function AulaAluno() {
     const [alunoData, setAlunoData] = useState([])
     const [aulasData, setAulasData] = useState([])
     const [modalInfoOpen, setModalInfoOpen] = useState(false)
-
+    const [filteredData, setFilteredData] = useState([])
 
     useEffect(() => {
         
@@ -45,15 +45,20 @@ export default function AulaAluno() {
         push('./rotas/alunos/')
     }
 
+    function filterData(id) {
+        setFilteredData(aulasData.filter(x => {return(x.id == id)}))
+        console.log(filteredData)
+    }
+
     return(
-        <div className="w-screen h-screen pt-[5.5rem] text-white flex flex-row justify-between">
+        <div className="w-screen h-screen pt-[4rem] md:pt-[5.5rem] text-white flex flex-row justify-between">
             <div className="flex flex-col items-center w-[100%] h-[100%]">
                 <div className="w-[100%] h-[4rem] bg-Cinza2 border-b-[1px] border-white">
                     <div className="flex flex-row w-[100%] items-start p-4">
                         <h1 onClick={() => linkarAlunos()} 
                         className="text-[1.2rem] font-bold mr-4 cursor-pointer text-BordaHeader hover:text-white transition-[0.1s]">Alunos</h1>
                         <h1 className="text-[1.2rem] font-bold mr-4 cursor-pointer">-</h1>
-                        <h1 className="text-[1.2rem] font-bold mr-4 cursor-pointer text-BordaHeader hover:text-white transition-[0.1s]">Exercicios</h1>
+                        <h1 className="text-[1.2rem] font-bold mr-4 cursor-pointer text-BordaHeader hover:text-white transition-[0.1s]">Exercicios {alunoData[0] && alunoData[0].nome}</h1>
                     </div>
                 </div>
                 <Butao texto="Adicionar"/>
@@ -64,8 +69,9 @@ export default function AulaAluno() {
                             return(
                                 <div key={aulas.id}>
                                 <AulasCard 
-                                funcao={() => setModalInfoOpen(!modalInfoOpen)}
-                                dia={aulas.dia_semana} treino1={aulas.nomeExercicio1} treino2={aulas.nomeExercicio2} treino3={aulas.nomeExercicio3}/>
+                                funcao={() => {filterData(aulas.id), setModalInfoOpen(!modalInfoOpen)}}
+                                dia={aulas.dia_semana} treino1={aulas.nomeExercicio1} treino2={aulas.nomeExercicio2} treino3={aulas.nomeExercicio3}
+                                />
                                 </div>
                             )
                         })
@@ -73,7 +79,15 @@ export default function AulaAluno() {
                 </div>
                 {
                     modalInfoOpen && 
-                    <ModalInfo/>
+                    <ModalInfo fechar={() => setModalInfoOpen(false)}
+                        dia={filteredData[0].dia_semana}
+                        exercicio1nome={filteredData[0].nomeExercicio1}
+                        exercicio1={filteredData[0].exercicio_1}
+                        exercicio2nome={filteredData[0].nomeExercicio2}
+                        exercicio2={filteredData[0].exercicio_2}
+                        exercicio3nome={filteredData[0].nomeExercicio3}
+                        exercicio3={filteredData[0].exercicio_3}
+                    />
                 }
             </div>
             <InfoAulaAluno nome={alunoData[0] && alunoData[0].nome} 
